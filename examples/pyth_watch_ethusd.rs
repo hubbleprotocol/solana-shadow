d049e6b5b21283a9eca02c89af6b0f76bbc5e92f
Karim Agha <karim.dev@gmail.com>
2021-09-30 11:21:38 +0000

    updated examples

--- examples/pyth_watch_ethusd.rs
+++ examples/pyth_watch_ethusd.rs
@@ -14,32 +14,44 @@ async fn main() -> Result<()> {
   // https://pyth.network/developers/accounts/
   let ethusd = "JBu1AL4obBcCMqKBBxhpWCNUt136ijcuMZLFvTP7iWdB".parse()?;
   let btcusd = "GVXRSBjFk6e6J3NbVPXohDJetcTjaeeuykUpbQF8UoMU".parse()?;
+  let solusd = "H6ARHf6YXhGYeQfUzQNGk6rDNnLBQKrenN712K4AQJEG".parse()?;
 
   // create an offline shadow of the on-chain data.
   // whenever the data change on-chain those changes
   // will be reflected immediately in this type.
-  let shadow = BlockchainShadow::new_for_accounts(
-    &vec![ethusd, btcusd],
-    Network::Mainnet,
-  )
-  .await?;
+  let mut shadow =
+    BlockchainShadow::new_for_accounts(&vec![ethusd, btcusd], Network::Mainnet)
+      .await?;
 
   println!("this example will print prices of ETH and BTC every 3 seconds");
+  println!("starting on the 15th second it will also print SOL prices");
+  println!();
 
   // iterate over the offline shadow of the account
   // everytime any account is accessed, then its contents
   // will reflect the latest version on-chain.
-  for _ in 0.. {
-    // access through a method
+  for i in 0.. {
+    // access the most recent snapshot of an account
     let ethacc = &shadow.get_account(&ethusd).unwrap();
     let ethprice = cast::<Price>(&ethacc.data).agg.price;
 
-    // access through index operator
     let btcacc = &shadow.get_account(&btcusd).unwrap();
     let btcprice = cast::<Price>(&btcacc.data).agg.price;
 
     println!("ETH/USD: {}", ethprice);
     println!("BTC/USD: {}", btcprice);
+
+    if i == 5 {
+      // dynamically add new accounts to mirror
+      shadow.add_account(&solusd).await?;
+    }
+
+    if i > 5 {
+      let solacc = &shadow.get_account(&solusd).unwrap();
+      let solprice = cast::<Price>(&solacc.data).agg.price;
+      println!("SOL/USD: {}", solprice);
+    }
+
     println!();
 
     tokio::time::sleep(Duration::from_secs(3)).await;
