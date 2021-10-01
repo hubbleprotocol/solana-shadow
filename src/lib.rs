a93b61ac2f0dc3261a363fe65e09beadd95eac80
Karim Agha <karim.dev@gmail.com>
2021-10-01 23:50:03 +0000

    cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
 mod blockchain;
 mod error;
 mod network;
-mod program;
 mod sync;
 mod message;
 
@@ -9,11 +8,3 @@ pub use blockchain::BlockchainShadow;
 pub use error::{Error, Result};
 pub use network::Network;
 pub use solana_sdk::pubkey::Pubkey;
-
-#[cfg(test)]
-mod tests {
-  #[test]
-  fn it_works() {
-    assert_eq!(2 + 2, 4);
-  }
-}
